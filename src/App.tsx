a3e48bfe72b069d0216731b3084c6af4d7087b83
Maksim Guido <maksguido@gmail.com>
2023-02-15 11:21:33 +0300

    feat: delete image and fix styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import { useState } from 'react';
-import { Preview, ProgressBar } from './components';
-import { ContextProvider } from './context';
-import { Upload } from './modules';
-import { IImage } from './types';
+import { deleteObject } from "firebase/storage";
+import { useState } from "react";
+import { Preview, ProgressBar } from "./components";
+import { ContextProvider } from "./context";
+import { Upload } from "./modules";
+import { IImage } from "./types";
 
 function App() {
   const [images, setImage] = useState<IImage[]>([]);
 
-  const onDeleteImage = (id: string) => {
-    setImage(images.filter((item) => item.id !== id));
+  const onDeleteImage = async (image: IImage) => {
+    if (image.ref) {
+      await deleteObject(image.ref);
+    }
+    setImage(images.filter((item) => item.id !== image.id));
   };
 
   const getPercentage = () => {
@@ -39,19 +43,18 @@ function App() {
       });
       return 0;
     }
-console.log((currentSize * 100) / maxSize);
 
     return (currentSize * 100) / maxSize;
   };
 
   return (
     <ContextProvider value={{ images, setImage }}>
-      <div className='w-full max-w-3xl mx-auto px-2'>
-        <div className='flex gap-2 pt-32 pb-5'>
+      <div className="w-full max-w-3xl mx-auto px-2">
+        <div className="flex gap-2 pt-32 pb-5">
           <ProgressBar percentage={getPercentage()} />
           <Upload />
         </div>
-        <Preview images={images} isCloud={true} onDelete={onDeleteImage} />
+        <Preview images={images} onDelete={onDeleteImage} />
       </div>
     </ContextProvider>
   );
--- src/components/ui/Preview.tsx
+++ src/components/ui/Preview.tsx
@@ -1,46 +1,45 @@
-import { IImage } from '../../types';
-import { Delete, Cloud } from '../icons';
+import { IImage } from "../../types";
+import { Delete } from "../icons";
 
 interface Props {
   images: IImage[];
-  isCloud: boolean;
-  onDelete: (id: string) => void;
+  onDelete: (image: IImage) => void;
 }
 
 export const Preview = (props: Props) => {
-  const { images, isCloud, onDelete } = props;
+  const { images, onDelete } = props;
 
   return (
     <div>
-      <div className='grid grid-cols-4 grid-rows-6 gap-2'>
+      <div className="grid grid-cols-4 grid-rows-6 gap-2">
         {images.map((image) => {
           return (
             <div
-              className='border rounded-md p-1 flex flex-col relative shadow-md min-h-[200px]'
+              className="border rounded-md p-1 flex flex-col relative shadow-md h-[200px]"
               key={Math.random()}
             >
               {image.url && (
-                <div className='flex-1 rounded-md'>
+                <div className="flex-1 rounded-md h-[150px] w-full">
                   <img
                     src={image.url as string}
-                    className='object-cover h-full rounded-md'
+                    className="object-cover h-full w-full rounded-md"
                     alt={image.name}
                   />
                 </div>
               )}
-              {isCloud && (
-                <span className='absolute top-0.5 left-0.5 w-5 h-5 flex items-center justify-center'>
-                  <Cloud />
+              {image.ref && (
+                <span className="absolute top-[-5px] left-[-5px] px-2 py-0.5 shadow-md shadow-blue-600 rounded-full flex text-white text-xs bg-blue-500">
+                  В облаке
                 </span>
               )}
               <span
-                onClick={() => onDelete(image.id)}
-                className='absolute top-0.5 right-0.5 w-5 h-5 cursor-pointer flex items-center justify-center'
+                onClick={() => onDelete(image)}
+                className="absolute top-0.5 right-0.5 w-5 h-5 cursor-pointer flex items-center justify-center"
               >
                 <Delete />
               </span>
-              <p className='text-sm truncate'>{image.name}</p>
-              <p className='text-sm font-semibold text-right'>
+              <p className="text-sm truncate">{image.name}</p>
+              <p className="text-sm font-semibold text-right">
                 Size: {image.size} kB
               </p>
             </div>
